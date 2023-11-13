ae60b15e90e67e15e4c335ee5ffd46b4349a2b9a
murilobauerc <murilo.bauer@sumup.com>
2023-11-13 20:19:39 -0300

    🔧 chore(next.config.js): add missing semicolons to improve code style and readability 📦 chore(package.json): update next package version to ^14.0.2 and add newsapi package as a dependency 📝 feat(pages/api/external/newsApi.js): add API route to fetch news from NewsAPI and handle errors and responses 📝 feat(utils/errorHandler.js): add error handling functions to handle API errors and responses

---
+++ pages/api/external/newsApi.js
@@ -0,0 +1,40 @@
+import NewsAPI from "newsapi";
+import { handleError, handleNewsResponse } from "../../../utils/errorHandler";
+
+export default async function handler(req, res) {
+  if (req.method !== "GET") {
+    handleError(res, "Method Not Allowed");
+    return;
+  }
+
+  const apiKey = process.env.NEXT_PUBLIC_NEWS_API_KEY;
+
+  if (!apiKey) {
+    handleError(res, "Chave da API não configurada corretamente");
+    return;
+  }
+
+  try {
+    const newsApiResponse = await fetchNews(apiKey);
+    handleNewsResponse(res, newsApiResponse);
+  } catch (error) {
+    handleError(res, error.message);
+  }
+}
+
+const fetchNews = async (apiKey) => {
+  const newsapi = new NewsAPI(apiKey);
+
+  try {
+    const queryOptions = {
+      q: "Gremio OR Inter",
+      language: "pt",
+      sortBy: "relevancy",
+      domains:
+        "clicrbs.com.br, ge.globo.com/rs/futebol/times/gremio/, ge.globo.com/rs/futebol/times/internacional/",
+    };
+    return await newsapi.v2.everything(queryOptions);
+  } catch (error) {
+    throw new Error(`Erro na consulta à NewsAPI: ${error.message}`);
+  }
+};
