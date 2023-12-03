0180e6b6ce7d2275f6e8df2e935efc6769270bec
jonaszeferino <jonaszeferino@gmail.com>
2023-12-03 01:43:51 -0300

    authentican almost done - login, reset password ok, next step, put verificarion login to see content in all pages

--- pages/admin/password-reset.jsx
+++ pages/admin/password-reset.jsx
@@ -1,5 +1,5 @@
 import react, { useState, useEffect } from "react";
-import { supabase } from "../utils/supabaseClient";
+import { supabase } from "../utils/supabaseClientAdmin";
 import {
   Box,
   Heading,
