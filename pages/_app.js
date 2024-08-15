e875b6f6523cafd804bb28a12939a1fea49b8930
jonaszeferino <jonaszeferino@gmail.com>
2024-08-15 15:33:04 -0300

    with GA

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import '@/styles/globals.css';
 import { ChakraProvider } from '@chakra-ui/react';
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 export default function App({ Component, pageProps }) {
   return (
     <ChakraProvider>
       <Component {...pageProps} />
+      <GoogleAnalytics gaId="G-YB2QWRRN12" />
     </ChakraProvider>
   );
 }
